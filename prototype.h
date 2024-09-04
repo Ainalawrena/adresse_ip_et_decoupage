f4548638a954c5a225d96f7eb020455887fd0875
Ainalawrena <167605889+Ainalawrena@users.noreply.github.com>
2024-09-04 18:53:59 +0200

    Add files via upload

---
+++ prototype.h
@@ -0,0 +1,12 @@
+void separe(char *chaine);
+void sous_reseau_binaire(int sous_reseau,char *chaine1);
+void separer(char *chaine,char *chaine_ip,int sous_reseau);
+int binaire(int *tab);
+void decimal(int x,char *chaine);
+int* reseau(char *chaine1,char *chaine2);
+int* boardcast(int *chaine1,int *chaine2);
+void binaire_reverse(int sous_reseau,char *chaine1) ;
+void execution_boucle(char *chaine);
+void calculerSousReseaux(int *ip, int masque, int nombreSousReseaux);
+void afficherAdresse(int *octets) ;
+void execution_total();
